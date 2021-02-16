b65e9acbe286f3784cd64c3b8801312fb8a00af1
Patrick Mackle <pimackle@ucdavis.edu>
2021-02-15 20:46:45 -0800

    bruteforce implementation twosumpy. will update

--- p1twosum.py
+++ p1twosum.py
@@ -1,1 +1,8 @@
-# comment
+# Naive implementation
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, outer in enumerate(nums):
+            for j, inner in enumerate(nums[i+1:], i+1):
+                sum = outer + inner
+                if sum == target:
+                    return [i, j]
